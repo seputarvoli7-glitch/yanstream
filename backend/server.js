2f5620397e9accc6c8e1f08e697bf5353a11767d
seputarvoli7-glitch <seputarvoli7@gmail.com>
2026-07-21 12:31:06 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        status: "YANSTREAM Backend Online"
+    }));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server berjalan di port", PORT);
+});
