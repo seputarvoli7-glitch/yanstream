74edc22bffb906f075c33e071aba9ee88f706098
seputarvoli7-glitch <seputarvoli7@gmail.com>
2026-07-21 19:25:59 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 const ADMIN = {
   username: "admin",
   password: "yanstream123"
