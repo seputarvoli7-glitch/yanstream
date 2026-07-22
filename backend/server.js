287037ccb57a5dd1ab2ca4b241397cedd8e78952
seputarvoli7-glitch <seputarvoli7@gmail.com>
2026-07-22 08:05:33 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/uploads", express.static("uploads"));
 const storage = multer.diskStorage({
     destination: "uploads/",
     filename: (req, file, cb) => {
